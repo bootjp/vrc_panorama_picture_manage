bd419159747177f5e500c6e6dc40fecea2864a08
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2019-10-03 15:50:01 +0900

    fix redirect manage system

--- cli/main.go
+++ cli/main.go
@@ -1,45 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
 	"github.com/garyburd/redigo/redis"
-	"github.com/golang/go/src/os"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
 	// Routes
-	e.GET("/", handler)
+	// ex) /resource_key
+	// ex) /r/resource_key
+	e.GET("/:key", handler)
+	e.GET("/r/:key", handler)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-
 func handler(c echo.Context) error {
-	k :=c.Path()
+	key := c.Param("key")
 	r := redisConnection()
-	s, err := redis.String(r.Do("GET", k))
+	s, err := redis.String(r.Do("GET", key))
 	if err != nil {
-		fmt.Println(err)
+		// redirect resource not found.
+		log.Println(err)
+		return c.NoContent(204)
 	}
-
 	return c.Redirect(302, s)
 }
 
 func redisConnection() redis.Conn {
 	host := os.Getenv("REDIS_HOST")
 
-	c, err := redis.Dial("tcp", host + ":6379")
+	c, err := redis.Dial("tcp", host+":6379")
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 	return c
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/bootjp/vrc_panoprama_picture_manage
+
+go 1.12
+
+require (
+	github.com/garyburd/redigo v1.6.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.10
+)
